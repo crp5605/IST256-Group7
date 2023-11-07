e6161139c17c0f48533dd6e03922f113fcf500a4
Colby Gleason <cjg5966@psu.edu>
2023-11-07 16:03:02 -0500

    adventure

---
+++ FinalProject.DAL/Models/AdventuresWorkingSettings.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.DAL.Models
+{
+	public class AdventureWorksSettings
+	{
+		public string categoryUri { get; set; }
+        public string productsUri {get;set ;}
+        public string suppliersUri {get; set;}
+	}
+}
